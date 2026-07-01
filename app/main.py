63de2788e6069b2aba064c29a3836fce2cba50e7
SanketHol <sanket.hol23@vit.edu>
2026-07-02 00:44:28 +0530

    Add SQLAlchemy user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+# from fastapi import FastAPI
+# from app.routers import home
+# from app.database import Base, engine
+# from app.models.user import User
+
+# app = FastAPI()
+
+# app.include_router(home.router)
+
+
 from fastapi import FastAPI
-from app.routers import home
+
+from app.database import Base, engine
+from app.models.user import User
+from app.routers.home import router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(home.router)
+app.include_router(router)
