ddfdf38aa4f3584c4774cfd54d54c45510c51b89
SanketHol <sanket.hol23@vit.edu>
2026-07-01 23:33:13 +0530

    Add request validation for user registration

--- app/routers/home.py
+++ app/routers/home.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from app.schemas.user import UserRegister
 
 router = APIRouter()
 
@@ -7,4 +8,12 @@ router = APIRouter()
 def home():
     return {
         "message": "Welcome to DevConnect 🚀"
+    }
+
+
+@router.post("/register")
+def register(user: UserRegister):
+    return {
+        "message": "User Registered Successfully",
+        "user": user
     }
---
+++ app/schemas/user.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserRegister(BaseModel):
+    full_name: str = Field(min_length=3, max_length=50)
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=32)
