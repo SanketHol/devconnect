2ea1244a87144ef460d8be4f90259a67f4aa1efb
SanketHol <sanket.hol23@vit.edu>
2026-07-18 01:53:38 +0530

    Started Creating Productions Ready Platform Basic

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+import App from "./App";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import queryClient from "./lib/queryClient";
+
+import { Toaster } from "react-hot-toast";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+    <React.StrictMode>
+
+        <QueryClientProvider client={queryClient}>
+
+            <App />
+
+            <Toaster
+                position="top-right"
+            />
+
+        </QueryClientProvider>
+
+    </React.StrictMode>
+
+);
---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<h1>Home</h1>}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
